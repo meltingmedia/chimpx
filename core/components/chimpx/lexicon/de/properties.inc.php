47ad892c6d975b01c0af8dfa773e606e4cd91dd4
smooth-graphics <info@novolo.de>
2011-06-07 13:50:28 +0200

    - translated into german
    Signed-off-by: smooth-graphics <info@novolo.de>

---
+++ core/components/chimpx/lexicon/de/properties.inc.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * chimpx
+ *
+ * Copyright 2011 by Romain Tripault <romain@melting-media.com>
+ *
+ * chimpx is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * chimpx is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * chimpx; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package chimpx
+ */
+/**
+ * Properties German Lexicon Entries for chimpx
+ * translated by Anselm Hannemann (http://anselm.novolo.de/)
+ * @package chimpx
+ * @subpackage lexicon
+ */
+// listCampaigns snippet
+$_lang['prop_chimpx.limit_desc'] = 'Anzahl an Kampagnen, die pro Seite angezeigt werden sollen.';
+$_lang['prop_chimpx.outputseparator_desc'] = 'Ein String um Zeilen zu trennen.';
+$_lang['prop_chimpx.tpl_desc'] = 'Der zu verwendende Chunk für jede Zeile der Kampagnen.';
+$_lang['prop_chimpx.offset_desc'] = 'Die Anzahl an Elementen pro Seite.';
+$_lang['prop_chimpx.listId_desc'] = 'Die Listen ID um Kampagnen zurückzugeben.';
+$_lang['prop_chimpx.status_desc'] = 'Der Kampagnenstatus, der erfasst werden soll. Verfügbare Status: "sent", "save", "paused", "schedule", "sending", "" (entspricht allen).';
+
+// chimpxSubscribe snippet
+$_lang['prop_chimpx.formTpl_desc'] = 'Chunk mit dem Abonnieren-Formular.';
+$_lang['prop_chimpx.debug_desc'] = 'Schalte Debug-Meldungen an.';
+$_lang['prop_chimpx.errorMsg_desc'] = 'Die anzuzeigende Nachricht für Besucher, wenn die Abonnierung fehl schlägt.';
+$_lang['prop_chimpx.successMsg_desc'] = 'Die anzuzeigende Nachricht für Besucher, wenn die Abonnierung erfolgreich war.';
